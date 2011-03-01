74d9f85da7574b8b463fd1893779f382c0bc31ea
PowerKiKi <adrien.crivelli@gmail.com>
2011-03-01 20:12:39 +0100

    removed 'comment are closed'

--- grace/comments.php
+++ grace/comments.php
@@ -28,16 +28,6 @@
 		<div class="alignleft"><?php previous_comments_link() ?></div>
 		<div class="alignright"><?php next_comments_link() ?></div>
 	</div>
- <?php else : // this is displayed if there are no comments so far ?>
-
-	<?php if ('open' == $post->comment_status) : ?>
-		<!-- If comments are open, but there are no comments. -->
-
-	 <?php else : // comments are closed ?>
-		<!-- If comments are closed. -->
-		<p class="nocomments">Comments are closed.</p>
-
-	<?php endif; ?>
 <?php endif; ?>
 
 
