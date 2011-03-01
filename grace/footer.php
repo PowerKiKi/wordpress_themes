70bb5c3b8dc23bb479bee7f475ca755cc38f1d58
PowerKiKi <adrien.crivelli@gmail.com>
2011-03-01 20:21:32 +0100

    smaller footer

--- grace/footer.php
+++ grace/footer.php
@@ -1,6 +1,6 @@
   <div id="footer">
     <p><a href="<?php echo get_option('home'); ?>/"><?php bloginfo('name'); ?></a> is proudly powered by	<a href="http://wordpress.org/">WordPress</a> | All Content &copy; <?php bloginfo('name'); ?></p>
-	<p><a href="http://7879.co.uk/downloads/grace-wordpress-photoblog-theme/" title="Free WordPress Photoblog Theme">Grace Photoblog Theme</a> Designed by <a href="http://7879.co.uk/" title="Small Business Web Marketing">7879</a></p>
+	<p>original design by <a href="http://7879.co.uk/" title="Small Business Web Marketing">7879</a>, <a href="https://github.com/PowerKiKi/wordpress_themes">modified by PowerKiKi</a></p>
   </div>
 
 </div> <!-- page -->
